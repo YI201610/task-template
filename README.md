a37358c40b0dbdd6439cd6cedf36e1e3dbaa842d
Yuji Imamura <YI201610@users.noreply.github.com>
2019-03-17 00:43:35 +0900

    シェルスクリプトを追加

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # task-template
-WIP: Unified Processを意識して、各案件に対するフォルダ, ファイルセットを生成する
+
+WIP: Unified Processを意識して、各案件に対応する作業フォルダや作業ファイルを生成
+
+(i) いったんiOS担当観点
+
+## usage
+
+`issue_name` には、案件名を入力する
+
+```
+./generate-template.sh <issue_name>
+```
---
+++ generate-template.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+    echo "usage: ./generate-template.sh <issue_name>"
+    exit
+fi
+
+# 注意: =と引数の間にスペースを入れないこと
+export itemName=$1
+export testCaseInstallPath=testSpecs/
+
+mkdir ${itemName}
+cd ${itemName}
+
+touch ${itemName}_DISCIPLINE.md
+{
+    echo "#" ${itemName} 
+	echo " "
+	echo "## 方向付け（モデリング, 顧客との対話）"
+	echo " "
+	echo "- 分析(※Android動作確認も必須): 7.14 %"
+	echo "- 設計: 14.28 %"
+	echo "- 実装+単体試験: 21.42 %"
+	echo "- ログ要件対応設計: 26.0 %"
+	echo "- （チケット起票）: 27.0 %"
+	echo " "
+	echo "## 推敲 （モデリング, 顧客との対話）"
+	echo " "
+	echo "- 分析(※Android動作確認も必須): 28.56 %"
+	echo "- 設計: 35.7 %"
+	echo "- 実装+単体試験: 42.84 %"
+	echo "- デザインレビュー(UI関連案件では必ず実行する): 49.98 %"
+	echo "- 試験項目作成: 57.12 %"
+	echo "- ログ要件実装: 60.0 %"
+	echo " "
+	echo "## 組み立て"
+	echo " "
+	echo "- 実装+単体試験: 64.26 %"
+	echo "- コードレビュー: 71.4 %"
+	echo "- 最終デザインレビュー: 78.54 %"
+	echo "- 試験実施: 85.68 %"
+	echo " "
+	echo "## 移行"
+	echo " "
+	echo "- featureマージ: 92.82 %"
+	echo "- 配置: 99.96 %"
+
+} > ${itemName}_DISCIPLINE.md
+
+touch ${itemName}_MEMO.md 
+
+mkdir download
+mkdir docs
+mkdir error-logs
+mkdir logs
+mkdir ipa
+mkdir testSpecs
+
+echo "[✓] directory: ${itemName} created."
+
+touch ${itemName}_TestCase.md 
+mv ${itemName}_TestCase.md ${testCaseInstallPath}
+
+echo "[✓] Test case: ${testCaseInstallPath} installed."
+
+exit
+
